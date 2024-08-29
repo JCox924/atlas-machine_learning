c55c3548b5db7e42e2611b74cc3aba10ad0a2aa9
JCox924 <joshua.cox@atlasschool.com>
2024-08-28 23:01:26 -0500

    add documentation

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+"""
+    Calculates shape of a matrix
+
+    Args:
+        matrix (list): matrix or multi-dimensional array to find shape of
+    Returns:
+        list: list of ints reflecting the depth of that index
+"""
+
 
 def matrix_shape(matrix):
     shape = []
