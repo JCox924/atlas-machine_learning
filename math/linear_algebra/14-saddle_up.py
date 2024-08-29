27b660fa636eb35672d31e5a5ba27283b19925d2
JCox924 <joshua.cox@atlasschool.com>
2024-08-29 14:37:38 -0500

    saddle up!

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""saddling up"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+
+    :param mat1: martix1
+    :param mat2: martix2
+    :return: product of mat1 and mat2
+
+    """
+    return np.matmul(mat1, mat2)
