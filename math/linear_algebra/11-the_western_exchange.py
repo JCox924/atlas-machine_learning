0c17e7a850b13aef80f21c65f77a820ab9f8d14f
JCox924 <joshua.cox@atlasschool.com>
2024-08-29 13:56:09 -0500

    add 11

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+""" find transpose using numpy"""
+
+
+def np_transpose(matrix):
+    """
+
+    :param matrx: 2D array
+    :return: transpose of matrix
+
+    """
+    return matrix.T
