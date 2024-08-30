065ebef10f7d58ec7920869400bec456de17603e
JCox924 <joshua.cox@atlasschool.com>
2024-08-29 22:33:33 -0500

    init commit

---
+++ math/plotting/0-line.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+def line():
+    """
+    line(void)
+    :return: Line PyPlot
+    """
+    x = np.arange(0, 11)
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+    plt.plot(x, y, color='#FF0000')
+    plt.show()
