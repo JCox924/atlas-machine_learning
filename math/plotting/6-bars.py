4d12673da3304582f7f87cb80d37cdfc0939fed5
JCox924 <joshua.cox@atlasschool.com>
2024-09-02 14:35:09 -0500

    add final plot file

---
+++ math/plotting/6-bars.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+def bars():
+    np.random.seed(5)
+    fruit = np.random.randint(0, 20, (4,3))
+    plt.figure(figsize=(6.4, 4.8))
+
+    names = ['Farrah', 'Fred', 'Felicia']
+
+    colors = ['#FF0000', '#FFFF00', '#FF8000', '#FFE5B4']
+
+    labels = ['Apples', 'Bananas', 'Oranges', 'Peaches']
+
+    width = 0.5
+
+    indices = np.arange(len(names))
+
+    floor = np.zeros(len(names))
+
+    for i in range(fruit.shape[0]):
+        plt.bar(indices, fruit[i], width=width, bottom=floor, color=colors[i], label=labels[i])
+        floor = fruit[i]
+
+    plt.ylabel('Quantity of Fruits')
+    plt.title('Number of Fruit per Person')
+    plt.xticks(indices, names)
+    plt.yticks(np.arange(0, 81, 10))
+    plt.ylim(0, 80)
+
+    plt.legend(loc=1)
+    plt.show()
