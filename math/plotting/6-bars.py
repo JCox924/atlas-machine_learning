ec72ae6e3d5fcc681b3bc8104b70cb937ad7ccfb
JCox924 <joshua.cox@atlasschool.com>
2024-09-04 23:52:59 -0500

    fix floor overwrite

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -26,8 +26,8 @@ def bars():
     plt.ylabel('Quantity of Fruits')
     plt.title('Number of Fruit per Person')
     plt.xticks(indices, names)
-    plt.yticks(np.arange(0, 80, 10))
+    plt.yticks(np.arange(0, 81, 10))
     plt.ylim(0, 80)
 
-    plt.legend(loc=1)
+    plt.legend(loc='best')
     plt.show()
