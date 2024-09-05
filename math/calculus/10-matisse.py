ee1d1c4fbf608ce7ecea2da57ea9ab75be257a3d
JCox924 <joshua.cox@atlasschool.com>
2024-09-04 23:34:30 -0500

    integrated

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def poly_derivative(poly):
-    if not isinstance(poly, list) or not all(isinstance(c, (int, float)) for c in poly):
+    if not isinstance(poly, list) or len(poly) == 0 or not all(isinstance(c, (int, float)) for c in poly):
         return None
     if len(poly) == 1:
         return [0]
