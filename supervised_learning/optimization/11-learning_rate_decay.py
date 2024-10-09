036cf448fa2a1266a5c720e6b2795cf9c04aff36
JCox924 <joshua.cox@atlasschool.com>
2024-10-09 12:12:53 -0500

    Add learning rate decay functions using TensorFlow and manual calculation
    This commit introduces two separate implementations for learning rate decay. One leverages TensorFlow's `InverseTimeDecay` for automatic decay handling, and the other provides a manual calculation method for exponential decay. This ensures flexibility for different scenarios in gradient descent optimization.

---
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Module 11-learning_rate_decay contains:
+    functions:
+        learning_rate_decay(alpha, decay_rate, global_step, decay_step)
+"""
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """
+    Updates the learning rate using exponential decay.
+        The new learning rate is given by:
+            alpha / (1 + decay_rate * (global_step // decay_step))
+    Args:
+        alpha: the learning rate
+        decay_rate: the weight used to determine the rate at which alpha will decay
+        global_step: the number of passes of gradient descent that have elapsed
+        decay_step: the number of passes of gradient descent that must elapse before alpha decays
+    Returns:
+        the updated value for alpha
+    """
+    new_alpha = alpha / (1 + decay_rate * (global_step // decay_step))
+
+    return new_alpha
---
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import tensorflow as tf
+"""
+Module contains:
+    functions:
+        learning_rate_decay(alpha, decay_rate, decay_step)
+"""
+
+
+def learning_rate_decay(alpha, decay_rate, decay_step):
+    """
+    Sets up the learning rate decay operation
+
+    Args:
+        alpha: the original learning rate
+        decay_rate: the weight used to determine the rate at which alpha will decay
+        decay_step: the number of passes of gradient descent
+            that should occur before alpha is decayed further
+    Returns:
+        the learning rate decay operation using TensorFlow's InverseTimeDecay function'
+    """
+
+    operation = tf.keras.optimizers.schedules.InverseTimeDecay(initial_learning_rate=alpha,
+                                                               decay_steps=decay_step,
+                                                               decay_rate=decay_rate,
+                                                               staircase=True)
+    return operation
