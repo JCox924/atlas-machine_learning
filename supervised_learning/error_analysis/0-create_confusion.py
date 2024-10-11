9e274a220e30dff5c83cc8423417f75c495c2ece
JCox924 <joshua.cox@atlasschool.com>
2024-10-11 15:23:44 -0500

    Add confusion matrix creation module and README for error analysis
    This commit introduces a new module, `0-create_confusion.py`, which contains a function to generate a confusion matrix from given labels and logits. Additionally, a `README.md` file is added to provide an overview and context for error analysis in machine learning.

---
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Module 0-create_confusion contains functions:
+    create_confusion_matrix(labels, logits)
+"""
+import numpy as np
+
+
+def create_confusion_matrix(labels, logits):
+    """
+    Args:
+        labels: one-hot matrix containing the correct labels
+        logits: ont-hot matrix containing the network's predictions'
+    Returns:
+        confusion matrix
+    """
+
+    classes = labels.shape[1]
+
+    confusion_matrix = np.zeros((classes, classes), dtype=int)
+
+    actual = np.argmax(labels, axis=1)
+    predictions = np.argmax(logits, axis=1)
+
+    for i in range(len(actual)):
+        confusion_matrix[actual[i], predictions[i]] += 1
+
+    return confusion_matrix
---
+++ supervised_learning/error_analysis/README.md
@@ -0,0 +1,16 @@
+# Error Analysis in Machine Learning
+
+
+## Intro
+
+Error Analysis is the process in which developers analysis the accuracy of a model
+to find the distribution of errors across different subsets of data.
+
+### Files
+
+| #                            | Name             |
+|------------------------------|------------------|
+| [0-](./0-create_confuion.py) | create_confusion |
+
+
+
