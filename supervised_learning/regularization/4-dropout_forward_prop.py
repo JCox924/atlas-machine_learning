371cb72d105e608829c5ba9a242e1c3b3c0b0197
JCox924 <joshua.cox@atlasschool.com>
2024-10-16 15:36:31 -0500

    Added helper function

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -6,6 +6,12 @@ Module 4-dropout_forward_prop contains functions:
 import numpy as np
 
 
+def softmax(Z):
+    """Softmax activation helper function for stability"""
+    exp_Z = np.exp(Z - np.max(Z, axis=0, keepdims=True))
+    return exp_Z / np.sum(exp_Z, axis=0, keepdims=True)
+
+
 def dropout_forward_prop(X, weights, L, keep_prob):
     """
     Conducts forward propagation using Dropout.
@@ -22,27 +28,22 @@ def dropout_forward_prop(X, weights, L, keep_prob):
     np.random.seed(0)
     cache = {}
     cache['A0'] = X
+    A = X
 
     for i in range(1, L + 1):
         W = weights['W' + str(i)]
         b = weights['b' + str(i)]
-        A_prev = cache['A' + str(i - 1)]
 
-        Z = np.matmul(W, A_prev) + b
+        Z = np.matmul(W, A) + b
 
-        if i != L:
+        if i == L:
+            A = softmax(Z)
+        else:
             A = np.tanh(Z)
-            D = (np.random.rand(*A.shape) < keep_prob).astype(float)
+            D = np.random.rand(A.shape[0], A.shape[1])
+            D = (D < keep_prob).astype(int)
             A *= D
             A /= keep_prob
             cache['D' + str(i)] = D
-            print(f"D{i} Dropou Mask:")
-            print(D)
-        else:
-            Z_shifted =  Z - np.max(Z)
-            t_exp = np.exp(Z_shifted)
-            A = t_exp / np.sum(t_exp)
-
         cache['A' + str(i)] = A
-
     return cache
