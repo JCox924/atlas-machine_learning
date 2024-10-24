3fd8c77bcafe073a6c74b022010008d1fcdfb797
JCox924 <joshua.cox@atlasschool.com>
2024-10-23 22:22:35 -0500

    fix: 11-config.py verbosity

--- supervised_learning/keras/12-test.py
+++ supervised_learning/keras/12-test.py
@@ -12,13 +12,17 @@ def test_model(network, data, labels, verbose=True):
 
     Args:
         network: The network model to test.
-        data: A numpy.ndarray of shape (m, nx) containing the input data to test the model with.
-        labels: A one-hot numpy.ndarray of shape (m, classes) containing the correct labels of data.
-        verbose: A boolean that determines if output should be printed during the testing process.
+        data: A numpy.ndarray of shape (m, nx) containing
+            the input data to test the model with.
+        labels: A one-hot numpy.ndarray of shape (m, classes)
+            containing the correct labels of data.
+        verbose: A boolean that determines if output should be
+            printed during the testing process.
 
     Returns:
-        tuple: The loss and accuracy of the model with the testing data, respectively.
+        tuple: The loss and accuracy of the model
+            with the testing data, respectively.
     """
     loss, accuracy = network.evaluate(data, labels, verbose=verbose)
 
-    return loss, accuracy
+    return [loss, accuracy]
