d1ac815b124b3eb944d81dd80f35366876b74a0e
JCox924 <joshua.cox@atlasschool.com>
2024-10-23 22:36:29 -0500

    fix: 1-convolve_grayscale_same.py to to mat addition to maintain mat shape

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -38,7 +38,7 @@ def convolve_grayscale_same(images, kernel):
 
     for i in range(h):
         for j in range(w):
-            convolved_images[:, i, j] = np.sum(
-                padded_images[:, i:i + kh, j:j + kw] * kernel, axis=(1, 2))
+            region = padded_images[:, i:i + kh, j:j + kw]
+            convolved_images[:, i, j] = np.sum(region * kernel, axis=(1, 2))
 
     return convolved_images
