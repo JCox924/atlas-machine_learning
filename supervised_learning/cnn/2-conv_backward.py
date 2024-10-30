306cbe06adcc3c718aa5e1cbcb62a3e21224d199
JCox924 <joshua.cox@atlasschool.com>
2024-10-30 14:09:15 -0500

    Fix: 2-conv_backward.py padding function for same
    Signed-off-by: JCox924 <joshua.cox@atlasschool.com>

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -5,41 +5,7 @@ Module for performing back propagation over a convolutional layer
 import numpy as np
 
 
-def pad_same(A_prev, kh, kw, sh, sw):
-    """
-    Performs same padding on images
-    Args:
-        A_prev: Input to be padded
-        kh: Filter height
-        kw: Filter width
-        sh: Stride height
-        sw: Stride width
-    Returns:
-        Padded input
-    """
-    h_prev, w_prev = A_prev.shape[1], A_prev.shape[2]
-
-    out_h = int(np.ceil(float(h_prev) / float(sh)))
-    out_w = int(np.ceil(float(w_prev) / float(sw)))
-
-    pad_h = max((out_h - 1) * sh + kh - h_prev, 0)
-    pad_w = max((out_w - 1) * sw + kw - w_prev, 0)
-
-    ph_top = pad_h // 2
-    ph_bottom = pad_h - ph_top
-    pw_left = pad_w // 2
-    pw_right = pad_w - pw_left
-
-    padded = np.pad(A_prev,
-                    ((0, 0),
-                     (ph_top, ph_bottom),
-                     (pw_left, pw_right),
-                     (0, 0)),
-                    mode='constant')
-    return padded, ph_top, ph_bottom, pw_left, pw_right
-
-
-def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
+def conv_backward(dZ, A_prev, W, padding="same", stride=(1, 1), mode='max'):
     """
     Performs back propagation over a convolutional layer
     Args:
@@ -58,8 +24,6 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
             containing the kernels for the convolution
             kh: filter height
             kw: filter width
-        b: numpy.ndarray of shape (1, 1, 1, c_new)
-            containing the biases
         padding: string that is either same or valid
             indicating the type of padding used
         stride: tuple of (sh, sw) containing the strides
@@ -68,53 +32,46 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     Returns:
         The gradients with respect to previous layer, kernels, and biases
     """
+    m, h_new, w_new, c_new = dZ.shape
     m, h_prev, w_prev, c_prev = A_prev.shape
-
-    kh, kw, _, c_new = W.shape
-
-    _, h_new, w_new, _ = dZ.shape
-
+    kh, kw, _, _ = W.shape
     sh, sw = stride
 
     dA_prev = np.zeros_like(A_prev)
     dW = np.zeros_like(W)
     db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
 
-    if padding == "valid":
-        padded = A_prev
-        ph_top, ph_bottom, pw_left, pw_right = 0, 0, 0, 0
-    elif padding == "same":
-        padded, ph_top, ph_bottom, pw_left, pw_right = pad_same(A_prev, kh, kw, sh, sw)
-
+    if padding == 'same':
+        ph = ((h_new - 1) * sh + kh - h_prev) // 2
+        pw = ((w_new - 1) * sw + kw - w_prev) // 2
+    elif padding == 'valid':
+        ph = pw = 0
     else:
         raise ValueError("padding must be 'same' or 'valid'")
 
-    dA_padded = np.zeros_like(padded)
+    A_prev_pad = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)), 'constant')
+    dA_prev_pad = np.pad(dA_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)), 'constant')
 
     for i in range(m):
-        A_prev_padded = padded[i]
-        dA_prev_padded = dA_padded[i]
+        a_prev_pad = A_prev_pad[i]
+        da_prev_pad = dA_prev_pad[i]
+        dz = dZ[i]
         for h in range(h_new):
             for w in range(w_new):
                 for c in range(c_new):
-                    h_start = h * sh
-                    h_end = h_start + kh
-                    w_start = w * sw
-                    w_end = w_start + kw
+                    vert_start = h * sh
+                    vert_end = vert_start + kh
+                    horiz_start = w * sw
+                    horiz_end = horiz_start + kw
 
-                    a_slice = A_prev_padded[h_start:h_end, w_start:w_end, :]
+                    a_slice = a_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :]
 
-                    dA_prev_padded[h_start:h_end, w_start:w_end, :] += (
-                            W[:, :, :, c] * dZ[i, h, w, c]
-                    )
-                    dW[:, :, :, c] += (
-                            a_slice * dZ[i, h, w, c]
-                    )
+                    da_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :] += W[:, :, :, c] * dz[h, w, c]
+                    dW[:, :, :, c] += a_slice * dz[h, w, c]
 
-    if padding == 'valid':
-        dA_prev = dA_padded
+    if ph == 0 and pw == 0:
+        dA_prev = dA_prev_pad
     else:
-        dA_prev = (
-                      dA_padded)[:, ph_top:ph_top + h_prev, pw_left:pw_left + w_prev, :]
+        dA_prev = dA_prev_pad[:, ph:-ph if ph != 0 else None, pw:-pw if pw != 0 else None, :]
 
     return dA_prev, dW, db
