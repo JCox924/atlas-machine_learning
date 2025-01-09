bee30a874dd0703cb06cbf695566f61660707d1c
JCox924 <joshua.cox@atlasschool.com>
2025-01-08 23:18:43 -0600

    fix square array check

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -19,7 +19,7 @@ def minor(matrix):
     if not isinstance(matrix, list) or not all(isinstance(row,list) for row in matrix):
         raise TypeError('matrix must be a list of lists')
 
-    if len(matrix) == 0 or any(isinstance(row, list) for row in matrix):
+    if len(matrix) == 0 or any(len(row) != len(matrix) for row in matrix):
         raise TypeError('matrix must be a non-empty square matrix')
 
     determinant = __import__('0-determinant').determinant
