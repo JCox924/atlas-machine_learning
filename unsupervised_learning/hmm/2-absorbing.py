13d9c106a31867c70240ef4b1bb913b7ee61956e
JCox924 <joshua.cox@atlasschool.com>
2025-02-03 22:38:22 -0600

    pep8 fix

--- unsupervised_learning/hmm/2-absorbing.py
+++ unsupervised_learning/hmm/2-absorbing.py
@@ -7,7 +7,7 @@ Module 2-absorbing contains
 import numpy as np
 
 
-def absorbing(P)-> bool:
+def absorbing(P) -> bool:
     """
     Args:
         P(np.ndarray):  square 2D array of shape (n, n) representing
@@ -15,22 +15,26 @@ def absorbing(P)-> bool:
     Returns:
         Returns: True if it is absorbing, or False on failure
     """
-    if not isinstance(P, np.ndarray) or P.ndim != 2 or P.shape[0] != P.shape[1]:
+    if (not isinstance(P, np.ndarray) or
+            P.ndim != 2 or
+            P.shape[0] != P.shape[1]):
         return False
-    if not np.allclose(P.sum(axis=1), 1, atol=1e-8) or np.any(P < 0) or not np.all(np.isfinite(P)):
+    if (not np.allclose(P.sum(axis=1), 1, atol=1e-8)
+            or np.any(P < 0)
+            or not np.all(np.isfinite(P))):
         return False
 
-    n =P.shape[0]
+    n = P.shape[0]
 
     absoring_states = []
 
     for i in range(n):
         row_no_i = np.concatenate((P[i, :i], P[i, i+1:]))
-        if np.isclose(P[i, i], 1, atol=1e-8) and np.allclose(row_no_i, 0, atol=1e-8):
+        if (np.isclose(P[i, i], 1, atol=1e-8)
+                and np.allclose(row_no_i, 0, atol=1e-8)):
             absoring_states.append(i)
         if len(absoring_states) == 0:
             return False
-
 
     def can_reach(i, v):
         if i in absoring_states:
