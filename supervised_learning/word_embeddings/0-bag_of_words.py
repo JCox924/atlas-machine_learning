28aef24003db733d93b1ba50c0e5aeeb68c4ed2a
JCox924 <joshua.cox@atlasschool.com>
2025-04-01 08:53:32 -0500

    attempt 2

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -4,8 +4,8 @@ Module 0-bag_of_words contains:
     functions:
         - bag_of_words(sentences, vocab=None)
 """
+import re
 import numpy as np
-
 
 def bag_of_words(sentences, vocab=None):
     """
@@ -15,31 +15,36 @@ def bag_of_words(sentences, vocab=None):
         sentences (list of str): The sentences to analyze.
         vocab (list of str, optional): A list of vocabulary words to use for the analysis.
                                        If None, all unique words from the sentences are used.
+                                       Words are normalized to lowercase and punctuation is removed.
 
     Returns:
         tuple: A tuple (embeddings, features) where:
             - embeddings (numpy.ndarray): An array of shape (s, f) where s is the number of sentences
               and f is the number of features. Each entry [i, j] contains the count of the j-th feature
               in the i-th sentence.
-            - features (list of str): The list of vocabulary words (features) used for the embeddings.
+            - features (list of str): The list of vocabulary words (features) used for the embeddings,
+              sorted in alphabetical order.
     """
+    processed_sentences = []
+    all_words = []
+    for sentence in sentences:
+        words = re.findall(r'\b[a-z]+\b', sentence.lower())
+        words = [word for word in words if word != 's']
+        processed_sentences.append(words)
+        all_words.extend(words)
+
     if vocab is None:
-        features_set = set()
-        for sentence in sentences:
-            words = sentence.split()
-            features_set.update(words)
-        features = sorted(features_set)
+        features = sorted(set(all_words))
     else:
         features = vocab
 
-    feature_index = {word: idx for idx, word in enumerate(features)}
+    feature_to_index = {word: idx for idx, word in enumerate(features)}
 
     embeddings = np.zeros((len(sentences), len(features)), dtype=int)
 
-    for i, sentence in enumerate(sentences):
-        words = sentence.split()
+    for i, words in enumerate(processed_sentences):
         for word in words:
-            if word in feature_index:
-                embeddings[i, feature_index[word]] += 1
+            if word in feature_to_index:
+                embeddings[i, feature_to_index[word]] += 1
 
     return embeddings, features
