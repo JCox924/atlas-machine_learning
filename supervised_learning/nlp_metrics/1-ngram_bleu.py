a7729daf320ef5187fafc4a03d54bd5ff9a9d003
JCox924 <joshua.cox@atlasschool.com>
2025-04-09 23:21:21 -0500

    pycode style fixes

--- supervised_learning/nlp_metrics/1-ngram_bleu.py
+++ supervised_learning/nlp_metrics/1-ngram_bleu.py
@@ -10,7 +10,7 @@ from collections import Counter
 
 def ngram_bleu(references, sentence, n):
     """
-    
+    calculates the n-gram BLEU score for a sentence
     :param references: list of reference translations
     :param sentence: list containing the model proposed sentence
     :param n: size of the n-gram to use for evaluation
