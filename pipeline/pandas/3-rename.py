e7ca96c72ae2548a22d6665d198fd88181081e47
JCox924 <joshua.cox@atlasschool.com>
2025-06-19 12:04:08 -0500

    Refactor: Rename 'Timestamp' column.

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -8,14 +8,17 @@ import pandas as pd
 
 
 def rename(df):
-    """Renames 'Timestamp' column to 'Datetime', converts to datetime, and filters columns.
+    """
+    Renames 'Timestamp' column to 'Datetime', converts
+        to datetime, and filters columns.
 
     Parameters:
     df : pandas.DataFrame
         DataFrame containing a 'Timestamp' column and a 'Close' column.
 
     Returns:
-        Modified DataFrame with only 'Datetime' (as datetime objects) and 'Close'.
+        Modified DataFrame with only 'Datetime'
+            (as datetime objects) and 'Close'.
     """
     df = df.rename(columns={'Timestamp': 'Datetime'})
     df['Datetime'] = pd.to_datetime(df['Datetime'], unit='s')
