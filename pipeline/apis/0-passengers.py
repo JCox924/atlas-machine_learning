a318e7deafab487783cc0717c6f95c1ac875e891
JCox924 <joshua.cox@atlasschool.com>
2025-06-25 23:53:39 -0500

    feat: Add starship availability module
    - Implemented `availableShips` function to query SWAPI and return starships based on passenger count.

---
+++ pipeline/apis/0-passengers.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Module that queries the SWAPI API to find starships
+    available for a given passenger count.
+"""
+import requests
+
+
+def availableShips(passengerCount):
+    """
+    Return a list of starship names that can hold
+        at least passengerCount passengers.
+
+    Args:
+        passengerCount (int): Number of
+            passengers to accommodate.
+
+    Returns:
+        list: Names of starships (str) that can
+            carry at least passengerCount passengers.
+    """
+    ships = []
+    url = 'https://swapi.dev/api/starships/'
+
+    while url:
+        response = requests.get(url)
+        if response.status_code != 200:
+            break
+        data = response.json()
+
+        for ship in data.get('results', []):
+            passengers = ship.get('passengers', '0').replace(',', '')
+            try:
+                capacity = int(passengers)
+            except ValueError:
+                continue
+            if capacity >= passengerCount:
+                ships.append(ship.get('name'))
+
+        url = data.get('next')
+
+    return ships
